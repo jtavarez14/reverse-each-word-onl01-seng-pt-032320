e6bd8015ae33c424d424f31f9a89b1707241723c
JeanCarlos Tavarez <jeancarlos.tavarez1@gmail.com>
2020-04-03 05:16:09 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,4 @@
-def reverse_each_word("Hello there, and how are you?")
+def reverse_each_word(string)
   reverese = ["Hello there, and how are you?"]
   reverse.each do |string|
     string.reverse 
